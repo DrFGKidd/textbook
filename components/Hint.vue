da010bad7f0ddf6eef9d7999dfdd65807c2c5af6
fgkidd <70215028+DrFGKidd@users.noreply.github.com>
2021-08-24 06:59:56 -0400

    Breaking things into SFC's
    Trying to break apart the components into more manageable chunks

---
+++ components/Hint.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data: function() {
+    return {
+      id: this.$parent.id
+    }
+  },
+  props: {
+    number: {type: String},
+    text:   {type: String},
+  },
+  components: {
+    Variable,
+    Blank,
+  },
+  methods: {
+    process: function(text) {
+      return "This is processed: "+text
+    },
+    parseText: function(text) {
+      let elements = []
+    }
+  },
+  render: function (createElement) {
+    return createElement("variable",
+    {
+        props: {
+          name: "pressure",
+          max: 500000,
+          min: 200000,
+          units: "Pa,atm"
+        }
+      }
+      )
+  }
+}
+</script>
---
+++ components/Variable.vue
@@ -0,0 +1,41 @@
+<template>
+  <span>\\( {{var_value}} \\ {{unit}} \\)</span>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: {
+    name: {type: String},
+    max: [Number, String],
+    min: [Number, String],
+    units: {type: String},
+  },
+  data: function () {
+    return {
+      cnv: this.$store.conversions,
+      str: this.$store[this.$parent.name]
+    }
+  },
+  computed: {
+    unit: function() {
+      this.units_array = this.units.split(",")
+      return this.units_array[Math.floor(Math.random()*this.units_array.length)]},
+    conversion: function() {
+      this.str[this.name+"_conversion"]=this.cnv[this.unit]/this.cnv[this.units_array[0]]
+      return this.cnv[this.unit]/this.cnv[this.units_array[0]]
+    },
+    sig: function() {
+      return Math.max(this.max.toString().replace(".","").length,this.min.toString().replace(".","").length)
+    },
+    var_value: function() {
+      let max = parseInt(this.max,10)
+      let min = parseInt(this.min,10)
+      this.str[this.name] = Math.random()*(max-min)+min
+      this.str[this.name+"_converted"]= Number((this.str[this.name]*this.conversion).toPrecision(this.sig))
+      return this.str[this.name+"_converted"]
+    },
+  },
+})
+</script>
