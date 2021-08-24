886d75fd799046a537ff6dd7ade51d7a04e6818e
fgkidd <70215028+DrFGKidd@users.noreply.github.com>
2021-08-24 18:06:59 -0400

    httpVueLoader?
    VueLoader to avoid webpack?

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 //app.component('problem0', {
 //numbers out of range, what to do if in wrong order?
 //For Vue 2
-import { Vue } from 'vue'
-import { App } from './App'
+//import  App  from './App.vue'
+httpVueLoader.register(Vue, './App.vue')
 
 const store = Vue.observable({
   conversions: {'N':1,'lb_f':.225,'m/s^2':1,'ft/s^2':3.28, 'kg':1, "Pa":1, "atm":0.00000987},
