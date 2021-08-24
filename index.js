4896c633bb4f632045c4405d135b85123bdfded9
fgkidd <70215028+DrFGKidd@users.noreply.github.com>
2021-08-24 17:49:25 -0400

    Getting setup is hard
    Just getting the file extensions correct

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //app.component('problem0', {
 //numbers out of range, what to do if in wrong order?
 //For Vue 2
-import { App } from './App.vue'
+import { App } from './App'
 
 const store = Vue.observable({
   conversions: {'N':1,'lb_f':.225,'m/s^2':1,'ft/s^2':3.28, 'kg':1, "Pa":1, "atm":0.00000987},
