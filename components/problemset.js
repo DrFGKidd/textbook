c72f68c0a0ec9d752938ae745b8a39fc84fd46fe
fgkidd <70215028+DrFGKidd@users.noreply.github.com>
2021-08-31 12:50:18 -0400

    Added problemset capabilities
    Made the Problemset have the ability to randomize the order and to choose only a subset of problems

--- components/problemset.js
+++ components/problemset.js
@@ -4,10 +4,44 @@ var ProblemSet = Vue.extend({
       type: String,
       required: true,
     },
+    number: {
+      type: [String, Number],
+      default: "all"
+    },
+    random: {
+      type: Boolean,
+      default: false
+    }
   },
   data: function () {
     return {
-      problems: this.$store.problemsets[this.problemkey]
+      all_problems: this.$store.problemsets[this.problemkey]
+    }
+  },
+  computed: {
+    problems: function() {
+      let probs = this.all_problems
+      if (this.random) {
+        probs = this.shuffle(probs)
+      }
+      if (!isNaN(this.number)) {
+        console.log(probs.length)
+        let num = Math.min(this.number,probs.length)
+        probs = probs.slice(0,num)
+      }
+      return probs
+    }
+  },
+  methods: {
+    shuffle: function(array) {
+      let currentIndex = array.length, randomIndex;
+      while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
+      }
+      return array;
     }
   },
   components: {
